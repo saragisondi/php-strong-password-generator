a216990f0caafb33b126e88474116f486178b9d0
Sara Gisondi <sara.gisondi@outlook.it>
2023-05-17 16:28:44 +0200

    add lentgh psw

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<?php ?>
+<?php
+  
+  
+  if (isset($_GET['password'])) {
+    var_dump($_GET['password']);
+  }
+  $password = $_GET['password'];
+
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -21,10 +30,10 @@
     <div class="row">
 
       <div class="col-3">
-        <form action="" method=""> 
+        <form action="index.php" method="GET"> 
           <label for="InputPassword" class="form-label">Password</label>
-          <input type="password" class="form-control" id="InputPassword">
-  
+          <input type="text" name="password" class="form-control" id="InputPassword">
+          <p>Lunghezza <?php echo strlen($password) ?> Caratteri</p>
           <div class="col my-3">
             <button type="submit" class="btn btn-success">Invia</button>
           </div>
